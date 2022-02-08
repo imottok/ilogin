81e1819a804f8d9e3851457721a6c16b8a7d40d9
imottok <help@imotto.co.kr>
2022-02-08 17:47:11 +0900

    Vue SSR 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export function createStore () {
+  const store = new Vuex.Store({
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+  })
+  return store;
+}
